e91a6c7b79be63680031abba70a06beb76e0eec4
imiebaka <triple07even@gmail.com>
2023-01-06 02:22:03 +0100

    megatron

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 npm run build
 cp /home/imiebaka/Documents/myPorfoilo/src/assets/Miebaka-Iwarri_CV.pdf /home/imiebaka/Documents/myPorfoilo/docs/assets
 cp /home/imiebaka/Documents/myPorfoilo/src/assets/images/miebaka-iwarri_cover-image-meta.jpg /home/imiebaka/Documents/myPorfoilo/docs/assets
+cp /home/imiebaka/Documents/myPorfoilo/CNAME /home/imiebaka/Documents/myPorfoilo/docs
 git add .
 git commit -m "megatron"
 git push
