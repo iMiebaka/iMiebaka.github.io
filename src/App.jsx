d247b05876b0c53be5fbf637547b84357c824923
imiebaka <triple07even@gmail.com>
2023-09-26 12:57:54 +0100

    404 fixes added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { LandingPage, Wallet } from "./pages";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/iMiebaka.github.io">
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/wallet" element={<Wallet />} />
